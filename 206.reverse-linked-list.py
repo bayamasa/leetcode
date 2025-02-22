bc7141983b2ddb1a223e2f3eaf8a7e0facf9a368
bayamasa <mapi0213mgt@gmail.com>
2025-02-22 10:05:42 +0900

    solved 206

---
+++ 206.reverse-linked-list.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=206 lang=python3
+#
+# [206] Reverse Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        previous = None
+        current = head
+        while current:
+            next_tmp = current.next
+            current.next = previous
+            previous = current
+            current = next_tmp
+        return previous
+        
+# @lc code=end
+
